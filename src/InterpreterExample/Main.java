42466e70895c8b395446226db1a7c6491ea06ba2
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-01-28 09:47:15 -0300

    Exemplo Interpreter

--- src/InterpreterExample/Main.java
+++ src/InterpreterExample/Main.java
@@ -3,7 +3,18 @@ package InterpreterExample;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Operador somar = new Somar(new Numero(1), new Numero(4));
+        System.out.println(somar.interpretar());
+        
+        Operador subtrair = new Subtrair(somar, new Numero(2));
+        System.out.println(subtrair.interpretar());
+        
+        Operador multiplicar = new Multiplicar(subtrair, somar);
+        System.out.println(multiplicar.interpretar());
+        
+        Operador dividir = new Dividir(new Numero(30), multiplicar);
+        System.out.println(dividir.interpretar());
 
 	}
 
---
+++ src/InterpreterExample/Operador.java
@@ -0,0 +1,5 @@
+package InterpreterExample;
+
+public interface Operador {
+	int interpretar();
+}
